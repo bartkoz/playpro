ff8568de0ec7f300a5f57220eba27bd5451124f9
bartkoz <bartmkoz@gmail.com>
2022-03-25 14:47:35 +0400

    make it better

--- playpro/settings.py
+++ playpro/settings.py
@@ -86,17 +86,17 @@ APPEND_SLASH = True
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'de7fogkt93j376',
-#         'USER': 'ytmsalmrcmlwto',
-#         'PASSWORD': 'd758bf0bfd4e55e9b100fa31d3482aa1c4630f59e13a60240bf348d3359ec9cc',
-#         'HOST': 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com',
-#         'PORT': '5432',
-#         'CONN_MAX_AGE': 2000,
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'de7fogkt93j376',
+        'USER': 'ytmsalmrcmlwto',
+        'PASSWORD': 'd758bf0bfd4e55e9b100fa31d3482aa1c4630f59e13a60240bf348d3359ec9cc',
+        'HOST': 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432',
+        'CONN_MAX_AGE': 2000,
+    }
+}
 DATABASES = {}
 db = dj_database_url.config(conn_max_age=2000)
 DATABASES['default'].update(db)
