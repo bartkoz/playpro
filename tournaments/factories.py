d7dd514a3b0dbeabd91c0417bc4690c439abf42a
bartkoz <bartmkoz@gmail.com>
2022-04-17 22:09:31 +0200

    factories

---
+++ tournaments/factories.py
@@ -0,0 +1,32 @@
+import factory
+from faker import Faker
+
+from tournaments.models import Tournament, TournamentTeam, TournamentTeamMember, TournamentGroup
+from users.factories import UserFactory
+from users.models import School
+
+
+class TournamentTeamFactory(factory.django.DjangoModelFactory):
+    school = School.objects.order_by('?').first()
+    tournament = Tournament.objects.order_by('?').first()
+    name = Faker(['en_US', ]).sentence(nb_words=3)
+    tournament_joined = True
+    captain = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = TournamentTeam
+
+
+class TournamentTeamMemberFactory(factory.django.DjangoModelFactory):
+    user = factory.SubFactory(UserFactory)
+    team = factory.SubFactory(TournamentTeamFactory)
+
+    class Meta:
+        model = TournamentTeamMember
+
+
+class TournamentGroupFactory(factory.django.DjangoModelFactory):
+    tournament = Tournament.objects.order_by('?').first()
+
+    class Meta:
+        model = TournamentGroup
