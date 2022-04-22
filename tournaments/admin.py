90f6a6dfab1a5daae72dbf31141be0225555d948
bartkoz <bartmkoz@gmail.com>
2022-04-22 19:18:48 +0200

    changes

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -6,7 +6,7 @@ from tournaments.models import (
     TournamentTeam,
     TournamentTeamMember,
     TournamentGroup,
-    TournamentMatch,
+    TournamentMatch, TournamentGame, TournamentGamePlatformMap, GamerTagChoice,
 )
 
 admin.site.register(TournamentPlatform)
@@ -15,3 +15,6 @@ admin.site.register(TournamentTeam)
 admin.site.register(TournamentTeamMember)
 admin.site.register(TournamentGroup)
 admin.site.register(TournamentMatch)
+admin.site.register(TournamentGame)
+admin.site.register(TournamentGamePlatformMap)
+admin.site.register(GamerTagChoice)
